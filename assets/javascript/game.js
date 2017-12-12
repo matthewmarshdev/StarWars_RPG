9f20a29c9e1653e3d2f53a4a59f727bb27dd01f7
Matthew <matthewmarshdev@gmail.com^C>
2017-12-11 21:40:40 -0700

    night

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Star Wars Attack game
+
+/** -Build object for each character
+   	-add all character objects to an array
+   	-if the select character button is clicked, move the html to the selected character area
+   	-if the select enemy button is clicked, move the html to the selected character area
+
+
+   	-maybe add a setTimeOut on the characters falling health as a super move. 
+   	-maybe add a bleed function on the characters that uses setInterval(function, time)
