0056294a28cac6a745078a5868c08a8a6ca3ef4f
matthewmarshdev <matthewmarshdev@gmail.com>
2017-12-17 13:35:29 -0700

    getting there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,28 +11,28 @@
 
 
 var ewok = {
-	name: 'ewok',
+    name: 'ewok',
     health: 85,
     maxPower: 30,
 
 };
 
 var jarjar = {
-	name: 'jarjar',
+    name: 'jarjar',
     health: 60,
     maxPower: 30,
 
 };
 
-var chewyandporg = {
-	name: 'chewyandporg',
+var porg = {
+    name: 'porg',
     health: 115,
     maxPower: 20,
 
 };
 
 var reddit = {
-	name: 'reddit',
+    name: 'reddit',
     health: 40,
     maxPower: 45,
 };
@@ -40,72 +40,66 @@ var reddit = {
 player = {};
 opponent = {};
 
-//array of player objects  
-var playerList = [ewok, jarjar, chewyandporg, reddit];
+var playerList = [ewok, jarjar, porg, reddit];
 
 //this method needs work, will essentially set the player and opponent variables based on button
 //clicks i will use the .appendto $('.divC').appendTo('.divB'); to do so 
 
-function selectPlayer(){
-	var pickedEwok = $('#chooseewok');
-	var fightEwok = $('#fightewok');
-	var pickedJarJar = $('#choosejarjar');
-	var fightJarJar = $('#fightjarjar');
-	var pickedChewyAndPorg = $('#choosechewyandporg');
-	var fightChewyAndPorg = $('#fightchewyandporg');
-	var pickedReddit = $('#choosereddit');
-	var fightReddit = $('#fightreddit');
+function selectPlayer() {
+    var pickedEwok = $('#chooseewok');
+    var fightEwok = $('#fightewok');
+    var pickedJarJar = $('#choosejarjar');
+    var fightJarJar = $('#fightjarjar');
+    var pickedChewyAndPorg = $('#chooseporg');
+    var fightChewyAndPorg = $('#fightporg');
+    var pickedReddit = $('#choosereddit');
+    var fightReddit = $('#fightreddit');
 }
 
+var determineAttack = (maxPower) => {
+    return Math.floor(Math.random() * maxPower);
 
-$(document).on("click", '.choosePlayer', function(){
-//$('.choosePlayer').on("click", function(){
-	for (i = 0; i < playerList.length; i++){
-		
-		//$(this).attr('data-name', '')
-		if(playerList[i].name === $(this).attr('data-name')){
-			player = playerList[i]; 
-			var movePlayer = $(`#${player.name}`);
-			$('#chosen-player').append(movePlayer);
-			$('#health').html(player.health);
-		}
-	}
-	/**if (this.id == 'chooseewok'){
-		pickedEwok.hide();
-		ewok = 
-		$('#chosen-player').append('#ewok');
-	}**/
+}
 
-});
+$(document).on("click", '.choosePlayer', function() {
+    //$('.choosePlayer').on("click", function(){
+    for (i = 0; i < playerList.length; i++) {
 
-$(document).on("click", '.chooseOpponent', function(){
-//$('.choosePlayer').on("click", function(){
-	for (i = 0; i < playerList.length; i++){
-		
-		//$(this).attr('data-name', '')
-		if(playerList[i].name === $(this).attr('data-name')){
-			player = playerList[i]; 
-			var movePlayer = $(`#${player.name}`);
-			$('#chosen-opponent').append(movePlayer);
-			$('#health').html(player.health);
-		}
-	}
-	/**if (this.id == 'chooseewok'){
-		pickedEwok.hide();
-		ewok = 
-		$('#chosen-player').append('#ewok');
-	}**/
+        //$(this).attr('data-name', '')
+        if (playerList[i].name === $(this).attr('data-name')) {
+            player = playerList[i];
+            var movePlayer = $(`#${player.name}`);
+            $('#chosen-player').append(movePlayer);
+            $('#health').html(player.health);
+        } 
+    }
 
 });
+
+$(document).on("click", '.chooseOpponent', function() {
+    //$('.choosePlayer').on("click", function(){
+    for (i = 0; i < playerList.length; i++) {
+
+        //$(this).attr('data-name', '')
+        if (playerList[i].name === $(this).attr('data-name')) {
+            player = playerList[i];
+            var movePlayer = $(`#${player.name}`);
+            $('#chosen-opponent').append(movePlayer);
+            $('#health').html(player.health);
+
+        }
+    }
+
+}); 
 
 
 //attack function, connected to attack button, hidden until players are selected
-function attack() {
+$(document).on("click", '#attackbtn', function(){
 
-    var attackButton = $('attack-button');
-    var gameMessage = $('message');
+    var attackButton = $('#attackbtn');
+    var gameMessage = $('#message');
     var restartButton = $('restart-button');
-    
+
 
     opponent.health -= player.maxPower;
     console.log()
@@ -116,48 +110,42 @@ function attack() {
 
     setTimeout(() => {
         var opponentAttack = determineAttack(opponent.power);
-        playerHealth -= opponentAttack;
+        player.health -= opponentAttack;
         attackButton.disabled = true;
     }, 1000);
-}
+});
 
 //fuction determines what the attack will land for 
-var determineAttack = (maxPower) => {
-    return Math.floor(Math.random() * maxPower);
 
-}
 
 //determines if the game is over or not
-function gameOver(health){
-	if (player.health === 0 && opponent.health >= 0) {
-	    $('#winorlose').html.show('YOU DIED!') ;
-	    attackButton.hidden = true;
-	    restartButton.hidden = false;
-	    return;
-	} else {
-	    $('#winorlose').html.show('YOU WIN!') ;
-	    attackButton.hidden = true;
-	    restartButton.hidden = false;
-	    return;
-	}
-	restart();
+function gameOver(health) {
+    if (player.health === 0 && opponent.health >= 0) {
+        $('#winorlose').html.show('YOU DIED!');
+        attackButton.hidden = true;
+        restartButton.hidden = false;
+        return;
+    } else {
+        $('#winorlose').html.show('YOU WIN!');
+        attackButton.hidden = true;
+        restartButton.hidden = false;
+        return;
+    }
+    restart();
 }
 
 //restart function for the game 
-function restart(){
-	var attackButton = $('#attack-button');
+function restart() {
+    var attackButton = $('#attack-button');
 
-	ewok.health = 85;
-	jarjar.health = 60;
-	chewyandporg.health = 115;
-	reddit.health = 40;
-	attackButton.disabled = false;
-	attackButton.hidden = false;
-	$('#restart-button').hidden = true;
-	$('#the-arena').empty();
-	printToScreen
+    ewok.health = 85;
+    jarjar.health = 60;
+    porg.health = 115;
+    reddit.health = 40;
+    attackButton.disabled = false;
+    attackButton.hidden = false;
+    $('#restart-button').hidden = true;
+    $('#the-arena').empty();
+    $('#chosen-opponent').append(movePlayer);
+ 
 }
-
-
-
-
